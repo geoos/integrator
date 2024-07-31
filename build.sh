db109497a59abbffbc1a3031e80a76fbca972b52
Jorge Jiménez (Jota) <42505799+JotaZonar@users.noreply.github.com>
2024-07-31 12:42:04 -0400

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-VERSION=1.01
-docker buildx build --push --platform linux/amd64,linux/arm64 -t docker.homejota.net/geoos/geoserver:latest -t docker.homejota.net/geoos/geoserver:$VERSION .
+VERSION=1.02
+docker buildx build --push --platform linux/amd64 -t docker.homejota.net/geoos/geoserver:latest -t docker.homejota.net/geoos/geoserver:$VERSION .
